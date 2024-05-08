f2cbbb7ce75de636ec4ceee1be1bbe1d3392dceb
nithara123 <kithsandinithara24@gmail.com>
2024-05-09 00:57:58 +0530

    Update indi.py

--- indi.py
+++ indi.py
@@ -5,32 +5,18 @@ import seaborn as sns
 
 # Custom CSS to style the sidebar
 sidebar_style = """
-    .sidebar .sidebar-content {
-        background-color: lightblue;
-        padding: 10px;
-        border-radius: 10px;
-    }
-    .sidebar .sidebar-content .sidebar-item a {
-        background-color: #ADD8E6;
-        color: black;
-        border-radius: 5px;
-        padding: 5px 10px;
-        margin-bottom: 5px;
-    }
-    .sidebar .sidebar-content .sidebar-item a:hover {
-        background-color: #87CEEB;
-    }
-    .sidebar .sidebar-content .sidebar-item a.selected {
-        background-color: #4169E1;
-        color: white;
-    }
+    background-color: lightblue;
+    padding: 10px;
+    border-radius: 10px;
 """
 
 # Apply the custom CSS to the sidebar
 st.markdown(
     f"""
     <style>
-    {sidebar_style}
+    .sidebar .sidebar-content {{
+        {sidebar_style}
+    }}
     </style>
     """,
     unsafe_allow_html=True
@@ -45,11 +31,20 @@ except Exception as e:
 # Sidebar menu options
 selected_option = st.sidebar.selectbox("Sales Overview", ["Sales by Category", "Sales Over Time", "Sales Quantity Distribution"])
 
+# Filter by Category and Region
+category_options = df['Category'].unique().tolist()
+selected_category = st.sidebar.selectbox("Select Category", category_options)
+filtered_df = df[df['Category'] == selected_category] if selected_category else df
+
+region_options = df['Region'].unique().tolist()
+selected_region = st.sidebar.selectbox("Select Region", region_options)
+filtered_df = filtered_df[filtered_df['Region'] == selected_region] if selected_region else filtered_df
+
 # Display selected visualization based on user choice
 if selected_option == "Sales by Category":
     st.subheader('Sales by Category')
     try:
-        sales_by_category = df.groupby('Category')['Quantity'].sum()
+        sales_by_category = filtered_df.groupby('Category')['Quantity'].sum()
         fig_bar = plt.figure(figsize=(8, 6))
         sales_by_category.plot(kind='bar')
         plt.xlabel('Category')
@@ -62,8 +57,8 @@ if selected_option == "Sales by Category":
 elif selected_option == "Sales Over Time":
     st.subheader('Sales Over Time')
     try:
-        df['Order Date'] = pd.to_datetime(df['Order Date'])
-        sales_over_time = df.groupby(pd.Grouper(key='Order Date', freq='M')).sum()['Sales']
+        filtered_df['Order Date'] = pd.to_datetime(filtered_df['Order Date'])
+        sales_over_time = filtered_df.groupby(pd.Grouper(key='Order Date', freq='M')).sum()['Sales']
         fig_line = plt.figure(figsize=(10, 6))
         sales_over_time.plot(kind='line')
         plt.xlabel('Order Date')
@@ -77,7 +72,7 @@ elif selected_option == "Sales Quantity Distribution":
     st.subheader('Sales Quantity Distribution')
     try:
         fig_hist = plt.figure(figsize=(8, 6))
-        df['Quantity'].plot(kind='hist', bins=10)
+        filtered_df['Quantity'].plot(kind='hist', bins=10)
         plt.xlabel('Quantity')
         plt.ylabel('Frequency')
         plt.title('Histogram - Sales Quantity Distribution')
@@ -86,15 +81,13 @@ elif selected_option == "Sales Quantity Distribution":
         st.error(f"Error creating histogram: {e}")
 
 # PRODUCT PERFORMANCE
-# Sidebar menu options
-selected_option_prod = st.sidebar.selectbox("Product Performance", ["Line Chart - Sales Quantity Over Time", "Discount vs Profit", "Bubble Chart - Profit vs Quantity"])
+selected_option_perf = st.sidebar.selectbox("Product Performance", ["Line Chart - Sales Quantity Over Time", "Discount vs Profit", "Bubble Chart - Profit vs Quantity"])
 
-# Display selected visualization based on user choice
-if selected_option_prod == "Line Chart - Sales Quantity Over Time":
+if selected_option_perf == "Line Chart - Sales Quantity Over Time":
     st.subheader('Line Chart - Sales Quantity Over Time')
     try:
-        df['Order Date'] = pd.to_datetime(df['Order Date'])
-        sales_quantity_over_time = df.groupby(pd.Grouper(key='Order Date', freq='M')).sum()['Quantity']
+        filtered_df['Order Date'] = pd.to_datetime(filtered_df['Order Date'])
+        sales_quantity_over_time = filtered_df.groupby(pd.Grouper(key='Order Date', freq='M')).sum()['Quantity']
         fig_line, ax = plt.subplots(figsize=(10, 6))
         sns.lineplot(x=sales_quantity_over_time.index, y=sales_quantity_over_time.values, ax=ax)
         ax.set_xlabel('Order Date')
@@ -104,11 +97,11 @@ if selected_option_prod == "Line Chart - Sales Quantity Over Time":
     except Exception as e:
         st.error(f"Error creating line chart: {e}")
 
-elif selected_option_prod == "Discount vs Profit":
+elif selected_option_perf == "Discount vs Profit":
     st.subheader('Discount vs Profit')
     try:
         fig_scatter, ax = plt.subplots(figsize=(10, 6))
-        sns.scatterplot(x='Profit', y='Discount', data=df, alpha=0.5, ax=ax)
+        sns.scatterplot(x='Profit', y='Discount', data=filtered_df, alpha=0.5, ax=ax)
         ax.set_xlabel('Profit')
         ax.set_ylabel('Discount')
         ax.set_title('Scatterplot - Discount vs Profit')
@@ -116,11 +109,11 @@ elif selected_option_prod == "Discount vs Profit":
     except Exception as e:
         st.error(f"Error creating scatterplot: {e}")
 
-elif selected_option_prod == "Bubble Chart - Profit vs Quantity":
+elif selected_option_perf == "Bubble Chart - Profit vs Quantity":
     st.subheader('Bubble Chart - Profit vs Quantity')
     try:
         fig_bubble, ax = plt.subplots(figsize=(10, 8))
-        sns.scatterplot(x='Profit', y='Quantity', size='Sales', data=df, alpha=0.5, ax=ax)
+        sns.scatterplot(x='Profit', y='Quantity', size='Sales', data=filtered_df, alpha=0.5, ax=ax)
         ax.set_xlabel('Profit')
         ax.set_ylabel('Quantity')
         ax.set_title('Bubble Chart - Profit vs Quantity')
@@ -128,16 +121,13 @@ elif selected_option_prod == "Bubble Chart - Profit vs Quantity":
     except Exception as e:
         st.error(f"Error creating bubble chart: {e}")
 
-
 # GEOGRAPHICAL INSIGHTS
-# Sidebar menu options for geographical insights
 selected_option_geo = st.sidebar.selectbox("Geographical Insights", ["Bar Graph - Sales by Region", "Line Graph - Sales Over Time by Region", "Heatmap - Sales by Category and Region"])
 
-# Display selected visualization based on user choice
 if selected_option_geo == "Bar Graph - Sales by Region":
     st.subheader('Bar Graph - Sales by Region')
     try:
-        sales_by_region = df.groupby('Region')['Sales'].sum()
+        sales_by_region = filtered_df.groupby('Region')['Sales'].sum()
         fig_bar = plt.figure(figsize=(10, 6))
         sales_by_region.plot(kind='bar')
         plt.xlabel('Region')
@@ -151,8 +141,8 @@ if selected_option_geo == "Bar Graph - Sales by Region":
 elif selected_option_geo == "Line Graph - Sales Over Time by Region":
     st.subheader('Line Graph - Sales Over Time by Region')
     try:
-        df['Order Date'] = pd.to_datetime(df['Order Date'])
-        sales_over_time_by_region = df.groupby([pd.Grouper(key='Order Date', freq='M'), 'Region']).sum()['Sales'].unstack()
+        filtered_df['Order Date'] = pd.to_datetime(filtered_df['Order Date'])
+        sales_over_time_by_region = filtered_df.groupby([pd.Grouper(key='Order Date', freq='M'), 'Region']).sum()['Sales'].unstack()
         fig_line = plt.figure(figsize=(10, 6))
         sales_over_time_by_region.plot(kind='line', ax=plt.gca())
         plt.xlabel('Order Date')
@@ -166,7 +156,7 @@ elif selected_option_geo == "Line Graph - Sales Over Time by Region":
 elif selected_option_geo == "Heatmap - Sales by Category and Region":
     st.subheader('Heatmap - Sales by Category and Region')
     try:
-        sales_by_category_and_region = df.pivot_table(index='Category', columns='Region', values='Sales', aggfunc='sum')
+        sales_by_category_and_region = filtered_df.pivot_table(index='Category', columns='Region', values='Sales', aggfunc='sum')
         fig_heatmap = plt.figure(figsize=(10, 6))
         sns.heatmap(sales_by_category_and_region, cmap='YlGnBu', annot=True, fmt='.1f')
         plt.title('Heatmap - Sales by Category and Region')
@@ -175,14 +165,12 @@ elif selected_option_geo == "Heatmap - Sales by Category and Region":
         st.error(f"Error creating heatmap: {e}")
 
 # CUSTOMER ANALYSIS
-# Sidebar menu options for customer analysis
 selected_option_cust = st.sidebar.selectbox("Customer Analysis", ["Pie Chart - Customer Segmentation", "Bar Graph - Sales by Customer Segment"])
 
-# Display selected visualization based on user choice
 if selected_option_cust == "Pie Chart - Customer Segmentation":
     st.subheader('Pie Chart - Customer Segmentation')
     try:
-        customer_segmentation = df['Segment'].value_counts()
+        customer_segmentation = filtered_df['Segment'].value_counts()
         fig_pie, ax = plt.subplots()
         ax.pie(customer_segmentation, labels=customer_segmentation.index, autopct='%1.1f%%', startangle=90)
         ax.axis('equal')
@@ -194,7 +182,7 @@ if selected_option_cust == "Pie Chart - Customer Segmentation":
 elif selected_option_cust == "Bar Graph - Sales by Customer Segment":
     st.subheader('Bar Graph - Sales by Customer Segment')
     try:
-        sales_by_segment = df.groupby('Segment')['Sales'].sum()
+        sales_by_segment = filtered_df.groupby('Segment')['Sales'].sum()
         fig_bar, ax = plt.subplots(figsize=(8, 6))
         sales_by_segment.plot(kind='bar')
         plt.xlabel('Customer Segment')
